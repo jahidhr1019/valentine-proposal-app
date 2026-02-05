66c1134873bb38b17bc4a174b5435371a8ae5b2b
MD Jahidul Hasan Rabbi <jahid.hijrat@gmail.com>
2026-02-05 06:36:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "eternal-echo-1-32204599-71682",
+  "appId": "1:38165820796:web:812dac6734184290d0e0f9",
+  "apiKey": "AIzaSyDI24yZvERELAma0amhgeTHxtYm4uQD8sM",
+  "authDomain": "eternal-echo-1-32204599-71682.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "38165820796"
+};
