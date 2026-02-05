fccf103fe2fcfd59e13c4a8ee73b1b63dded4bf6
jahidhr1019 <jahidhr1019@gmail.com>
2026-02-05 21:27:34 +0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Special+Elite&display=swap" rel="stylesheet" />
+        <meta name="google-site-verification" content="q0D3nYUa4ze6OSx_L-c_eHHPFmpjPHBz5qc89dn75-o" />
       </head>
       <body className="font-body antialiased">
         {children}
